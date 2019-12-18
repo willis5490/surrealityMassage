e4fd250e613ff0120d3df6033253212214549316
William Stearns <wcstearns@yahoo.com>
2019-12-18 14:38:34 -0700

    added logo

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -8,11 +8,16 @@ class Footer extends Component {
     return (
       <div>
             <nav id="footerBody" className="uk-navbar-container  uk-text-center " uk-navbar='true' uk-navbar="mode: click" uk-grid='true'>
-              <div className="uk-navbar-center ">
-
+              <div className="uk-navbar-left ">
                   <ul id="middlenav" className="uk-navbar-nav">    
-                  <li className=""><a id="mobile-links" href="/"><b><i>Surreality Massage Bitch</i></b></a></li>                                                                                                                                             
+                  <li className=""><a id="mobile-links" href="/"><b><i>Home</i></b></a></li>   
+                  <li className=""><a id="mobile-links" href="/About"><b><i>About</i></b></a></li>
+                  <li className=""><a id="mobile-links" href="/Contact"><b><i>Contact</i></b></a></li>                                                                                                                                          
                   </ul>
+              </div>
+
+              <div className="uk-navbar-right ">
+                <a href='/'><img src='../images/dark_logo_transparent.svg'  width="175" height="100" ></img></a>
               </div>
             </nav>
 
